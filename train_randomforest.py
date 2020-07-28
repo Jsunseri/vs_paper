4434c07a9a9094dce17d8aa416e67cd1378778ce
Jocelyn Sunseri <jss97@g012.csb.pitt.edu>
2020-07-27 23:03:53 -0400

    first version of random forest training script, barring fixes

--- train_randomforest.py
+++ train_randomforest.py
@@ -1,15 +1,45 @@
 #!/bin/env python
-import rdkit.AllChem as Chem
-import numpy as np
-import sys,os,glob
-import pandas as pd
+import sys,os,glob,gzip
 from argparse import ArgumentParser
-from rdkit import rdMolDescriptors
+from collections import namedtuple
+from joblib import dump
+
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import matplotlib.pyplot as plt
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-from pandas.api.types import is_numeric_dtype
-from joblib import dump
+
+from rdkit import Chem
+from rdkit.Chem import QED
+from rdkit.Chem import rdmolops
+from rdkit.Chem import rdMolDescriptors
+
+FoldData = namedtuple('FoldData', ['fnames', 'labels', 'fold_it'])
+FeaturizeOutput = namedtuple('FeaturizeOutput', ['features', 'failures'])
+FixedOutput = namedtuple('FixedOutput', ['labels', 'fold_it'])
+
+def unique_list(seq):
+    '''
+    Return new list with just the unique elements from the input, in the same
+    order as they first appeared in the input
+    '''
+    seen = set()
+    return [x for x in seq if x not in seen and not seen.add(x)]
+
+def exclusive_scan(iterable):
+    '''
+    Generate exclusive scan from an iterator
+    '''	
+    total = 0
+    yield total
+    for value in iterable:
+    	total += value
+    	yield total 
 
 def find_and_parse_folds(prefix, foldnums='', columns='Label,Affinity,Recfile,Ligfile', fit_all=False):
     '''
@@ -96,15 +126,16 @@ def find_and_parse_folds(prefix, foldnums='', columns='Label,Affinity,Recfile,Li
     fold_it = [get_indices(elems_per_df, idx) for idx in range(len(elems_per_df))]
 
     # get lig filenames
-    df = pd.concat(df_list, ignroe_index=True, sort=False)
+    df = pd.concat(df_list, ignore_index=True, sort=False)
     fnames = df['Ligfile'].to_numpy()
 
     # get y_values
     ycols = []
-    for col in df.columns:
-        if is_numeric_dtype(df[col]):
-            possible_yvals.append(col)
-    if not fit_all:
+    if fit_all:
+    	for col in df.columns:
+    	    if is_numeric_dtype(df[col]):
+    	        ycols.append(col)
+    else:
         if 'Affinity' in ycols:
             ycols = ['Affinity']
         elif 'Label' in ycols:
@@ -113,9 +144,9 @@ def find_and_parse_folds(prefix, foldnums='', columns='Label,Affinity,Recfile,Li
             assert 0, "Custom fit targets not implemented yet. Pass --fit_all '
             'or specify target column as 'Affinity'"
     labels = df[ycols].to_numpy()
-    return({'fnames':fnames, 'labels': labels, 'fold_it':fold_it})
+    return FoldData(fnames, labels, fold_it)
 
-def dude_descriptors(mol):
+def get_dude_descriptors(mol):
     '''
     Parameters
     ----------
@@ -136,7 +167,18 @@ def dude_descriptors(mol):
             + net charge
     '''
 
-def muv_descriptors(mol):
+    props = QED.properties(mol)
+    dude_descriptors = []
+    dude_descriptors.append(props.MW)
+    dude_descriptors.append(props.HBA)
+    dude_descriptors.append(props.HBD)
+    dude_descriptors.append(props.ROTB)
+    dude_descriptors.append(props.ALOGP)
+    dude_descriptors.append(rdmolops.GetFormalCharge(mol))
+
+    return dude_descriptors
+
+def get_muv_descriptors(mol):
     '''
     Parameters
     ----------
@@ -168,16 +210,42 @@ def muv_descriptors(mol):
             + number of ring systems
     '''
 
+    props = QED.properties(mol)
+    muv_descriptors = []
+    muv_descriptors.append(props.HBA)
+    muv_descriptors.append(props.HBD)
+    muv_descriptors.append(props.ALOGP)
+    muv_descriptors.append(mol.GetNumAtoms(onlyExplicit=False))
+    muv_descriptors.append(mol.GetNumHeavyAtoms())
+    type_list = [atom.GetAtomicNum() for atom in mol.GetAtoms()]
+    atom_counts = pd.DataFrame(type_list, columns=['type']).groupby('type').size().to_dict()
+
+    muv_descriptors.append(atom_counts[5])  # boron
+    muv_descriptors.append(atom_counts[35]) # bromine
+    muv_descriptors.append(atom_counts[6])  # carbon
+    muv_descriptors.append(atom_counts[17]) # chlorine
+    muv_descriptors.append(atom_counts[9])  # fluorine
+    muv_descriptors.append(atom_counts[53]) # iodine
+    muv_descriptors.append(atom_counts[7])  # nitrogen
+    muv_descriptors.append(atom_counts[8])  # oxygen
+    muv_descriptors.append(atom_counts[15]) # phosphorus
+    muv_descriptors.append(atom_counts[16]) # sulfur
+    
+    # TODO: will this actually get them all?
+    chiral_centers = Chem.FindMolChiralCenters(mol, includeUnassigned=True)
+    muv_descriptors.append(len(chiral_centers))
+    muv_descriptors.append(rdMolDescriptors.CalcNumRings(mol))
+
+    return muv_descriptors
+
 # we generate descriptors with rdkit and give the user the option of using the
 # DUD-E or MUV descriptors
-def generate_descriptors(mol_list, num_mols=0, data_root='', method='DUD-E'):
+def generate_descriptors(mol_list, data_root='', method='DUD-E'):
     '''
     Parameters
     ----------
     mol_list: array_like
         list of molecule filenames
-    num_mols: int
-        number of total mols expected
     data_root: str
         common path to join with molnames to generate full location
     method: {'DUD-E', 'MUV'}, optional
@@ -196,14 +264,23 @@ def generate_descriptors(mol_list, num_mols=0, data_root='', method='DUD-E'):
     # if a filename is repeated on contiguous lines and that filename is
     # multi-model, assume the lines correspond to those models and verify we
     # get the expected number.
-    count = 0
+    # TODO? currently not enforcing contiguity of examples from the same file, 
+    # which could lead to a mismatch between the labels and features
     failures = []
+    features = []
+    count = 0 # track total number of mols seen and compare with expected
     assert method == 'MUV' or method == 'DUD-E', "Only MUV or DUD-E molecular "
     "descriptors supported"
-    n_features = 6 if method == 'DUD-E' else 17
-    features = np.zeros((num_mols, n_features))
-    for molname in mol_list:
+
+    # how many mols do we expect per filename?
+    expected_mols_per_file = pd.DataFrame(mol_list, columns=['file']).groupby('file').size().to_dict()
+    # now get the (ordered) list of unique filenames
+    unique_files = unique_list(mol_list)
+
+    for molname in unique_files:
+        gzipped = False
         base,ext = os.path.splitext(molname)
+        fullname = os.path.join(data_root, molname)
         assert ext != '.gninatypes', "Sorry, no gninatypes support currently. "
         "Just pass the starting structure files; if you have multi-model SDFs, "
         "repeat the filename for each example derived from that file and the "
@@ -211,39 +288,106 @@ def generate_descriptors(mol_list, num_mols=0, data_root='', method='DUD-E'):
         if ext == '.gz':
             base,ext = os.path.splitext(base)
             assert ext == '.sdf', "Only SDFs can be gzipped for now."
+            gzipped = True
         if ext == '.pdb':
-            mol = Chem.MolFromPDBFile(molname)
+            mol = Chem.MolFromPDBFile(fullname)
             mols = [mol]
         elif ext == 'mol2':
-            mol = Chem.MolFromMol2File(molname)
+            mol = Chem.MolFromMol2File(fullname)
             mols = [mol]
         elif ext == '.sdf':
-            mols = Chem.ForwardSDMolSupplier(molname)
+            if gzipped:
+            	mols = Chem.ForwardSDMolSupplier(gzip.open(fullname))
+            else:
+            	mols = Chem.ForwardSDMolSupplier(fullname)
         elif ext == '.smi':
-            with open(molname, 'r') as f:
-                mols = [Chem.MolFromSmiles(line.strip()) for line in f]
+            with open(fullname, 'r') as f:
+                mols = [Chem.MolFromSmiles(line.split()[0]) for line in f]
         for i,mol in enumerate(mols):
             if mol is None:
                 print("Problem with molecule %d from file %s" %(i+1,molname))
                 failures.append(count)
             else:
                 if method == 'DUD-E':
-                    desc = dude_descriptors(mol)
+                    features.append(get_dude_descriptors(mol))
                 elif method == 'MUV':
-                    desc = muv_descriptors(mol)
+                    features.append(get_muv_descriptors(mol))
                 else:
                     # shouldn't get here because we already asserted above,
                     # buuuuut just in case
                     print("Unsupported molecular descriptor set %s" %method)
                     sys.exit()
-            features[i,:]
             count += 1
+        assert i == expected_mols_per_file[molname], "Got %d mols from %s but expected %d" %(i,molname, expected_mols_per_file[molname])
 
-    assert num_mols == count - len(failures), "len(labels) len(mols) mismatch; "
-    "ensure every mol has its own line in input types file. "
+    assert count == len(mol_list), "Saw %d mols but expected %d" %(count, len(mol_list))
+    features = np.asarray(features)
+    return FeaturizeOutput(features, failures)
+
+def delete_failure_indices_from_folds(failures, labels, fold_it):
+    '''
+    Parameters
+    ----------
+    failures: array_like
+        indices of examples to be deleted
+    labels: array_like
+        example labels
+    fold_it: iterable
+        iterable yielding fold indices
+    
+    Returns
+    ----------
+    labels: array_like
+        example labels with failures removed
+    fold_it: iterable
+        iterable with failures removed
+    '''
+    # delete labels associated with any mols we failed to parse
+    labels = np.delete(labels, failures, axis=0)
+ 
+    # also delete them from the iterator defining the folds...
+    # that iterator is actually a bunch of lists of indices;
+    # each index will appear in two train fold lists and one test fold, 
+    # and we can figure out which
+    test_sizes = [len(ttup[1]) for ttup in fold_it]
+    start_indices = [part_sum for part_sum in exclusive_scan(size for size in test_sizes)]
+    foldnums = list(range(len(test_sizes)))
+    index_groups = {}
+    for i in foldnums:
+        index_groups[i] = []
+    # first we'll partition the failed indices by test and train folds
+    for failed in failures:
+        fold = -1
+        for i,start in enumerate(start_indices[1:]):
+            if failed < start:
+                fold = i
+                break
+        assert fold != -1, "Failure index %d exceeds data size %d" %(failed, start_indices[-1])
+        index_groups[fold].append(failed)
+
+    # the fold we've noted is the test fold; concat the other folds to get the train fold
+    new_test_folds = {}
+    for fold,failed in index_groups.items():
+        # delete from test
+        failed = sorted(failed, reverse=True)
+        test_failed = [x-start_indices[fold] for x in failed]
+        test = fold_it[fold][1]
+        for index in test_failed:
+            del test[index]
+        new_test_folds[fold] = test
+
+    fold_it = []
+    for fold in foldnums:
+        other_folds = [num for num in foldnums if num != fold]
+        train = []
+        for num in other_folds:
+            train += new_test_folds[num]
+        fold_it.append((train,new_test_folds[fold]))
+
+    return FixedOutput(labels, fold_it)
 
 # use sklearn to cross validate, train, and optimize hyperparameters 
-def fit_and_cross_validate_model(X_train, y_train, fold_it):
+def fit_and_cross_validate_model(X_train, y_train, fold_it, param_grid, seed=42, classifier=False):
     '''
     Parameters
     ----------
@@ -253,12 +397,89 @@ def fit_and_cross_validate_model(X_train, y_train, fold_it):
         training labels
     fold_it: iterable
         iterable yielding fold indices
+    param_grid: dict
+        dictionary mapping hyperparams and values to define the grid search
+    seed: int
+        random seed to pass to Random Forest model
+    classifier: bool
+        whether to train a classifier instead of a regression model
 
     Returns
     ----------
     model: object
         fit model 
     '''
+
+    if classifier:
+        rf = RandomForestClassifier(random_state=seed, return_train_score=True)
+    else:
+        rf = RandomForestRegressor(random_state=seed, return_train_score=True)
+
+   grf = GridSearchCV(rf, param_grid, cv=fold_it, scoring='neg_mean_squared_error')
+   grf.fit(X_train, y_train)
+   return grf
+
+def plot_cv_results(results, gp1, gp2, classifier=False, figname='gridsearch_results.pdf'):
+    '''
+    Parameters
+    ----------
+    results: GridSearchCV results object
+    gp1: tuple
+        (name, grid parameter to plot on X axis)
+    gp2: tuple
+        (name, grid parameter to plot as distinct lines)
+    classifier: bool
+        whether a classifier or regressor model was trained
+    figname: str
+        name for output figure
+    '''
+
+    fig,ax = plt.subplots(figsize=(13, 13))
+    plt.title("GridSearchCV Results", fontsize=16)
+   
+    if classifier:
+        plot.ylabel('Score')
+        score_suffix = 'score'
+    else:
+        plot.ylabel('MSE') 
+        score_suffix = 'neg_mean_squared_error'
+
+    plt.xlabel(gp1[0])
+    
+    x = np.array(results['param_%s' %gp1[0]].data, dtype=float)
+    palette = ['g', 'k', 'goldenrod', 'deepskyblue', 'darkorchid', 'deeppink', 'sienna']
+    assert len(gp2[1]) < len(palette), 'Not enough distinct colors in palette'
+    this_palette = palette[:len(gp2[1])]
+    
+    for i,(param_val, color) in enumerate(zip(sorted(gp2[1]), this_palette)):
+        for sample, style in (('train', '--'), ('test', '-')):
+            sample_score_mean = results['mean_%s_%s' % (sample, score_suffix)]
+            sample_score_std = results['std_%s_%s' % (sample, score_suffix)]
+
+            sample_score_mean = np.array(sample_score_mean).reshape(len(grid_param_2),len(grid_param_1))
+            sample_score_std = np.array(sample_score_std).reshape(len(grid_param_2),len(grid_param_1))
+
+            ax.fill_between(x, sample_score_mean[i,:] - sample_score_std[i,:],
+                            sample_score_mean[i,:] + sample_score_std[i,:],
+                            alpha=0.1 if sample == 'test' else 0, color=color)
+            ax.plot(x, sample_score_mean[i,:], style, color=color,
+                    alpha=1 if sample == 'test' else 0.7,
+                    label='%s: %s (%s)' % (gp2[0], param_val, sample))
+    
+        best_index = np.nonzero(results['rank_test_%s' % score_suffix] == 1)[0][0]
+        best_score = results['mean_test_%s' % score_suffix][best_index]
+    
+        # Plot a dotted vertical line at the best score for that scorer marked by x
+        ax.plot([x[best_index], ] * 2, [0, best_score],
+                linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+    
+        # Annotate the best score for that scorer
+        ax.annotate('%0.2f' % best_score,
+                    (x[best_index], best_score + 0.005))
+    
+    plt.legend(loc='best')
+    plt.grid(False)
+    fig.savefig(figname, bbox_inches='tight')
 
 if __name__=='__main__':
     parser = ArgumentParser(description='Train a Random Forest model for '
@@ -273,6 +494,10 @@ if __name__=='__main__':
     parser.add_argument('-r', '--data_root', type=str, default='', 
             help='Common path to join with molnames to generate full location '
             'of files')
+    parser.add_argument('-m', '--method', type=str, default='DUD-E',
+            help='Descriptor set to use; options are "DUD-E" or "MUV"')
+    parser.add_argument('-s', '--seed', type=int, default=42,
+            help='Random seed, used for boostrapping and feature sampling')
     parser.add_argument('-f', '--fit_all', action='store_true', 
             help='Fit all numerical columns in types file with RFRegressor. '
             'Default is to use only affinity if available, label if it is not')
@@ -280,4 +505,38 @@ if __name__=='__main__':
             help='Output prefix for trained random forest pickle and train/test figs')
     args= parser.parse_args()
 
-    # dump(clf, 'filename.joblib')
+    fold_data = find_and_parse_folds(args.prefix, args.foldnums, args.columns, args.fit_all)
+    featurize_output = generate_descriptors(fold_data.fnames, args.data_root, args.method)
+   
+    if failures: 
+        out = delete_failure_indices_from_folds(featurize_output.failures, fold_data.labels, fold_data.fold_it)
+        labels = out.labels
+        fold_it = out.fold_it
+    else:
+        labels = fold_data.labels
+        fold_it = fold_data.fold_it
+
+    colnames = args.columns.split(',')
+    if args.fit_all or 'Affinity' in colnames:
+        classifier = False
+    else:
+        classifier = True
+
+    # TODO: maybe too much?
+    param_grid = {
+        'min_samples_split': [0.1, 0.25, 0.5, 1.0],
+        'min_samples_leaf': [0.1, 0.25, 0.5, 1.0],
+        'max_features': ['sqrt', 0.25, 0.5, 0.75, 1.0],
+        'n_estimators': [100, 200, 400, 750]
+    }
+    rf = fit_and_cross_validate_model(featurize_output.features, labels, fold_it, param_grid, args.seed, classifier)
+
+    if not args.outprefix:
+        args.outprefix = '%s_RF_%d' %(args.method, os.getpid())
+    dump(rf.best_estimator_, '%s.joblib' %args.outprefix)
+
+    params = list(param_grid.keys())
+    gp1 = params[0]
+    for gp2 in params[1:]:
+        plot_cv_results(rf.cv_results_, (gp1,param_grid[gp1]), (gp2,param_grid[gp2]), classifier, 
+                        '%s_%s_gridsearch_results.pdf' %(gp1,gp2))
